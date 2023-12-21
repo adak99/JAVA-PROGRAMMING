6598697950ee3f38cedb5be0ba52a972d17f6548
adak99 <soumyadipadak99@gmail.com>
2023-12-21 21:35:23 +0530

    How to add numbers using recursion

--- recursion_sum.java
+++ recursion_sum.java
@@ -1,16 +1,21 @@
-public class recursion_sum {
+public class recursion_sum { // method overloading
 
     int add(int num1, int num2) {
 
         int c = num1 + num2;
-        return c;
+
+        if (c != 0 || c == 0)
+            return c;
+
+        else
+            return add(num1, num2);
     }
 
     public static void main(String[] args) {
 
         recursion_sum r = new recursion_sum();
 
-        int result = r.add(6, 4);
+        int result = r.add(0, 0);
 
         System.out.println("Answer:- " + result);
     }
