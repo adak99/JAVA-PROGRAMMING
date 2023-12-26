ff6a4d198ca921c5416a9e7285678b9216eeb326
adak99 <soumyadipadak99@gmail.com>
2023-12-26 14:27:54 +0530

    print butterfly pattern

--- pattern_11.java
+++ pattern_11.java
@@ -1,3 +1,16 @@
+/*
+
+*      *
+**    **
+***  ***
+********
+********
+***  ***
+**    **
+*      *
+
+*/
+
 import java.util.Scanner;
 
 public class pattern_11 {
@@ -5,9 +18,45 @@ public class pattern_11 {
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("Enter a No;- ");
+        System.out.print("\nEnter a number:- ");
         int n = scan.nextInt();
 
         scan.close();
+
+        // upper half
+        for (int i = 1; i <= n; i++) {
+            // 1st part
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // spces
+            int space = 2 * (n - i);
+            for (int j = 1; j <= space; j++) {
+                System.out.print(" ");
+            }
+            // 2nd part
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // lower half
+        for (int i = n; i >= 1; i--) {
+            // 1st part
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // spces
+            int space = 2 * (n - i);
+            for (int j = 1; j <= space; j++) {
+                System.out.print(" ");
+            }
+            // 2nd part
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
