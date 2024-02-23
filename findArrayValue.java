217f8b7e7112d1f71b4d576cbc7fc7474ed9a2ea
Soumya ! <soumyadipadak99@gmail.com>
2024-02-23 12:27:21 +0530

    Find Max and Min valu in array

--- findArrayValue.java
+++ findArrayValue.java
@@ -1,29 +1,33 @@
-//find mini and max value in java
 
 import java.util.Scanner;
 
 public class findArrayValue {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+
+    public static void myMedhod() {
+        Scanner scan = new Scanner(System.in);
 
         System.out.print("Enter the size of the array: ");
-        int size = sc.nextInt();
+        int size = scan.nextInt();
 
         int number[] = new int[size];
 
         System.out.println("Enter array inputs: ");
         for (int i = 0; i < size; i++) {
-            number[i] = sc.nextInt();
+            number[i] = scan.nextInt();
         }
 
         int max = number[0];
         int min = number[0];
 
+        // or
+
+        // int max = Integer.MIN_VALUE;
+        // int min = Integer.MAX_VALUE;
+
         for (int i = 0; i < number.length; i++) {
             if (number[i] > max) {
                 max = number[i];
             }
-
             if (number[i] < min) {
                 min = number[i];
             }
@@ -32,8 +36,7 @@ public class findArrayValue {
         System.out.println("The max value of the array is: " + max);
     }
 
-    public void myMedhod() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'myMedhod'");
+    public static void main(String[] args) {
+        myMedhod();
     }
 }
