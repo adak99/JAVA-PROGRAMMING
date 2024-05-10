47fdaca9592486ed4bcccc34a5744b2da8c37219
Soumya ! <soumyadipadak99@gmail.com>
2024-05-10 23:55:54 +0530

    Find the tow array element are same in 2D array

---
+++ Array/check2DArratEqualorNot.java
@@ -0,0 +1,48 @@
+public class check2DArratEqualorNot {
+
+    private static void check2DArrayValue(int a[][], int b[][]) {
+
+        boolean equal = true;
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < b.length; j++) {
+                if (a[i][j] != b[i][j]) {
+                    equal = false;
+                }
+            }
+        }
+
+        if (equal) {
+            System.out.println("Both the element of the array is: Equal");
+        } else {
+            System.out.println("Both the element of the array is: Not equal");
+        }
+    }
+
+    public static void main(String args[]) {
+
+        int a[][] = { { 1, 2, 3 },
+                { 4, 5, 6 }, { 7, 8, 9 } };
+        int b[][] = { { 1, 2, 3 },
+                { 4, 5, 6 }, { 7, 8, 9 } };
+
+        System.out.print("Elements of a[]: ");
+        for (int i = 0; i < a.length; i++) {
+            System.out.println();
+            for (int j = 0; j < a.length; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+        }
+        System.out.println();
+        System.out.print("Elements of b[]: ");
+        for (int i = 0; i < b.length; i++) {
+            System.out.println();
+            for (int j = 0; j < b.length; j++) {
+                System.out.print(b[i][j] + " ");
+            }
+        }
+        System.out.println();
+        check2DArrayValue(a, b);
+
+    }
+}
