5aa9765d006f2a90be9567a72c237e8d29a3259e
Soumya ! <soumyadipadak99@gmail.com>
2024-05-11 12:12:40 +0530

    update

---
+++ String/StrBuilder.java
@@ -0,0 +1,42 @@
+
+//package JAVA-PROGRAMMING.String;
+//import java.util.Scanner;
+
+public class StrBuilder {
+    public static void main(String[] args) {
+        // StringBuilder sb = new StringBuilder("Tony");
+        // System.out.println(sb);
+
+        // // char at index
+        // // System.out.println(sb.charAt(0));
+
+        // // set char at index
+        // // sb.setCharAt(0, 'p');
+        // // System.out.println(sb);
+
+        // // // insetr function
+        // // sb.insert(0, 's');
+        // // System.out.println(sb);
+
+        // // inser char
+        // sb.insert(2, 'n');
+        // System.out.println(sb);
+
+        // // delete char
+        // // sb.delete(2, 3);
+        // // System.out.println(sb);
+
+        // sb.delete(2, 4);
+        // System.out.println(sb);
+
+        // // append
+
+        // append
+        StringBuilder sb = new StringBuilder("h");
+        sb.append("e");
+        sb.append("l");
+        sb.append("l");
+        sb.append("o");
+        System.out.println(sb);
+    }
+}
