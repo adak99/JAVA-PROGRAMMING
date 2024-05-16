8fd4e5d8a2b370cb7254f2ead1d0c41e58d654cd
Soumya ! <soumyadipadak99@gmail.com>
2024-05-16 09:39:14 +0530

    Reverse the array values

---
+++ Array/reverseArrayValue.java
@@ -0,0 +1,9 @@
+public class reverseArrayValue {
+    public static void main(String args[]) {
+
+        int arr[] = { 1, 2, 3, 4 };
+
+        for (int i = arr.length - 1; i >= 0; i--)
+            System.out.print(arr[i] + " ");
+    }
+}
