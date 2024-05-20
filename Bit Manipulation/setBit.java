5f3035f445c896b7e40cd42b68d29fdace11f803
Soumya ! <soumyadipadak99@gmail.com>
2024-05-20 19:41:45 +0530

    update

--- Bit Manipulation/setBit.java
+++ Bit Manipulation/setBit.java
@@ -1,11 +1,13 @@
 public class setBit {
     public static void main(String[] args) {
-
         int n = 5;
-        int pos = 2;
-        int bitMask = 2 << pos;
+        int position = 2;
+        int bitMask = 1 << position;
 
-        int newNumber = bitMask | n;
-        System.out.println(newNumber);
+        if ((bitMask & n) == 0) {
+            System.out.println("Bit was zero.");
+        } else {
+            System.out.println("bit was one.");
+        }
     }
 }
