5a841ba19cb47e0a359a258f7f6a0a7b1ea9724b
Soumya ! <soumyadipadak99@gmail.com>
2024-05-20 20:58:32 +0530

    update

---
+++ Bit Manipulation/updateBit.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class updateBit {
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the bit (Set/clear) : ");
+        int bit = scan.nextInt();
+        int n = 5; // 0101
+        int position = 1;
+        int bitMask = 1 << position;
+
+        if (bit == 1) {
+            int newNumber = bitMask | n;
+            System.out.println("The update bit is: " + newNumber);
+        } else {
+            int notBitMask = ~(bitMask);
+            int newNumber = notBitMask & n;
+            System.out.println("The update bit is: " + newNumber);
+        }
+    }
+}
