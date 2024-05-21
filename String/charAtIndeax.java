7c22216056b66205ec885c4a619faea5b1429f41
Soumya ! <soumyadipadak99@gmail.com>
2024-05-21 13:55:12 +0530

    update

---
+++ String/charAtIndeax.java
@@ -0,0 +1,9 @@
+//char at index
+
+public class charAtIndeax {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("hello");
+
+        System.out.println(sb.charAt(0));
+    }
+}
---
+++ String/syntaxOfStringBuilder.java
@@ -0,0 +1,6 @@
+public class syntaxOfStringBuilder {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("hello");
+        System.out.println(sb);
+    }
+}
