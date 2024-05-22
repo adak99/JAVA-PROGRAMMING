4eebf5ba365c3be8e4b0b7d3bda78258b02d8f89
Soumya ! <soumyadipadak99@gmail.com>
2024-05-22 12:36:14 +0530

    update

---
+++ String/append.java
@@ -0,0 +1,12 @@
+//Append
+
+public class append {
+    public static void main(String args[]) {
+        StringBuilder sb = new StringBuilder("h");
+        sb.append("e");
+        sb.append("l");
+        sb.append("l");
+        sb.append("o");
+        System.out.println(sb);
+    }
+}
