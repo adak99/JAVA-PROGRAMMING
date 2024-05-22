5b5b830d6e14904b688572c68b431c825c7f6879
Soumya ! <soumyadipadak99@gmail.com>
2024-05-22 11:33:12 +0530

    update

---
+++ String/setcharAtIndex.java
@@ -0,0 +1,15 @@
+//set char at index example2
+public class setCharAtIndexEx2 {
+    public static void main(String args[]) {
+        StringBuilder sb = new StringBuilder("nodejs"); // python
+
+        sb.setCharAt(0, 'p');
+        sb.setCharAt(1, 'y');
+        sb.setCharAt(2, 't');
+        sb.setCharAt(3, 'h');
+        sb.setCharAt(4, 'o');
+        sb.setCharAt(5, 'n');
+
+        System.out.println(sb);
+    }
+}
