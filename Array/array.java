bb00168f6239386ea95822aaa2b93c1a2724071d
Soumya ! <soumyadipadak99@gmail.com>
2024-05-28 00:34:59 +0530

    update

--- Array/array.java
+++ Array/array.java
@@ -3,7 +3,7 @@
 public class array {
     public static void main(String[] args) {
 
-        int arr[] = { 1, 2, 3 };
+        int arr[] = { 1, 2, 3, 4 };
         for (int i = 0; i < arr.length; i++)
             System.out.print(arr[i] + " ");
 
