49eaa02ea83dfe2ca1bf6605b4a15cb722de4753
Soumya ! <soumyadipadak99@gmail.com>
2024-06-01 20:01:15 +0530

    Find the both the array elements are same or not

--- Array/findArrayEquals.java
+++ Array/findArrayEquals.java
@@ -1,9 +1,5 @@
 public class findArrayEquals {
-    public static void main(String[] args) {
-        int a[] = { 1, 2, 3, 4, 5, 6 };
-        int b[] = { 1, 2, 3, 4, 5, 6 };
-
-        boolean arrayEquals = true;
+    private static void calculateValue(int a[], int b[], boolean arrayEquals) {
 
         if (a.length != b.length) {
             arrayEquals = false;
@@ -11,15 +7,40 @@ public class findArrayEquals {
             for (int i = 0; i < a.length; i++) {
                 if (a[i] != b[i]) {
                     arrayEquals = false;
-                    break;
                 }
             }
         }
 
         if (arrayEquals) {
-            System.out.println("Array are equal.");
+            System.out.println("Both the array vale is equla. ");
+
+            for (int i = 0; i < a.length; i++) {
+                System.out.print(a[i] + " ");
+            }
+            System.out.println();
+            for (int i = 0; i < b.length; i++) {
+                System.out.print(b[i] + " ");
+            }
         } else {
-            System.out.println("Array not equal.");
+            System.out.println("Both the array elements are not equal.");
+
+            for (int i = 0; i < a.length; i++) {
+                System.out.print(a[i] + " ");
+            }
+            System.out.println();
+            for (int i = 0; i < b.length; i++) {
+                System.out.print(b[i] + " ");
+            }
         }
+
+    }
+
+    public static void main(String args[]) {
+
+        int a[] = { 1, 2, 3, 4 };
+        int b[] = { 1, 2, 3, 4 };
+        boolean arrayEquals = true;
+
+        calculateValue(a, b, arrayEquals);
     }
 }
