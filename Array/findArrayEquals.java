5eee08f0976d375c9900c68d073530b1feef67e6
Soumya ! <soumyadipadak99@gmail.com>
2024-06-01 20:31:55 +0530

    Find the both the array elements are same or not

--- Array/findArrayEquals.java
+++ Array/findArrayEquals.java
@@ -1,46 +1,48 @@
+import java.util.Scanner;
+
 public class findArrayEquals {
-    private static void calculateValue(int a[], int b[], boolean arrayEquals) {
+    private static void calculateValue(int a[], int b[]) {
+        boolean arrayEquals = true;
 
         if (a.length != b.length) {
             arrayEquals = false;
         } else {
             for (int i = 0; i < a.length; i++) {
-                if (a[i] != b[i]) {
+                if (a[i] == b[i]) {
                     arrayEquals = false;
                 }
             }
         }
 
         if (arrayEquals) {
-            System.out.println("Both the array vale is equla. ");
-
-            for (int i = 0; i < a.length; i++) {
-                System.out.print(a[i] + " ");
-            }
-            System.out.println();
-            for (int i = 0; i < b.length; i++) {
-                System.out.print(b[i] + " ");
-            }
+            System.out.println("Both the array elements is same.");
         } else {
-            System.out.println("Both the array elements are not equal.");
-
-            for (int i = 0; i < a.length; i++) {
-                System.out.print(a[i] + " ");
-            }
-            System.out.println();
-            for (int i = 0; i < b.length; i++) {
-                System.out.print(b[i] + " ");
-            }
+            System.out.println("Both the array elemnets is not same.");
         }
-
     }
 
     public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
 
-        int a[] = { 1, 2, 3, 4 };
-        int b[] = { 1, 2, 3, 4 };
-        boolean arrayEquals = true;
+        System.out.print("Enter the size of the array of a[]: ");
+        int siezA = scan.nextInt();
+        System.out.print("Enter the size of the array of b[]: ");
+        int siezB = scan.nextInt();
 
-        calculateValue(a, b, arrayEquals);
+        int a[] = new int[siezA];
+        int b[] = new int[siezB];
+
+        System.out.print("Enter the value of a[]: ");
+        for (int i = 0; i < siezA; i++) {
+            a[i] = scan.nextInt();
+        }
+
+        System.out.print("Enter the value of b[]: ");
+        for (int i = 0; i < siezA; i++) {
+            b[i] = scan.nextInt();
+        }
+
+        calculateValue(a, b);
+        scan.close();
     }
 }
