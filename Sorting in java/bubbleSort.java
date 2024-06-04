136deb1c3e10e959e17cb4a1069dd063f1e65611
Soumya ! <soumyadipadak99@gmail.com>
2024-06-04 11:06:37 +0530

    Bubble sort in java

--- Sorting in java/bubbleSort.java
+++ Sorting in java/bubbleSort.java
@@ -1,24 +1,26 @@
+//Shorting the arrya in assending order using bubble short 
 public class bubbleSort {
-    public static void main(String[] args) {
-        int arr[] = { 4, 1, 3, 2, 0 };
 
-        int i = 0;
-        while (i < arr.length - 1) {
-            int j = 0;
-            while (j < arr.length - i - 1) {
+    public static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 7, 9, 5, 8, 4, 2, 3, 1, 0 };
+
+        // Bubble short
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
-                j++;
             }
-            i++;
         }
-
-        int k = 0;
-        while (k < arr.length) {
-            System.out.print(arr[k] + " ");
-        }
+        printArray(arr);
     }
 }
