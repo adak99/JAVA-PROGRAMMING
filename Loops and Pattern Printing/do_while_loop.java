c3c16cafc4ce5d6c6275aa9af38dd1422445a3db
Soumya ! <soumyadipadak99@gmail.com>
2024-06-18 09:51:20 +0530

    update

--- Loops and Pattern Printing/do_while_loop.java
+++
@@ -1,9 +0,0 @@
-public class do_while_loop {
-    public static void main(String args[]) {
-        int i = 0;
-        do {
-            System.out.println(i);
-            i++;
-        } while (i < 5);
-    }
-}
