6390a364cb6520ce20eacfbe50f5e9859aefb321
Soumya ! <soumyadipadak99@gmail.com>
2024-06-18 09:49:56 +0530

    Do while loop

--- Loops and Pattern Printing/do_while_loop.java
+++ Loops and Pattern Printing/do_while_loop.java
@@ -2,7 +2,7 @@ public class do_while_loop {
     public static void main(String args[]) {
         int i = 0;
         do {
-            System.out.println("hello");
+            System.out.println(i);
             i++;
         } while (i < 5);
     }
