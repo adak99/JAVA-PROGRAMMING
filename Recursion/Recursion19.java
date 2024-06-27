a8bf047bcb8f5d4ac9022ce2245640cf6829b515
Soumya ! <soumyadipadak99@gmail.com>
2024-06-27 23:12:15 +0530

    Place Tiles of size 1xm in a floor of size nxm

---
+++ Recursion/Recursion19.java
@@ -0,0 +1,26 @@
+// Place Tiles of size 1xm in a floor of size nxm
+public class Recursion19 {
+
+    public static int placeTiles(int n, int m) {
+        // base case
+        if (n == m) {
+            return 2;
+        }
+        if (n < m) {
+            return 1;
+        }
+
+        // vertically
+        int verticalPlacement = placeTiles(n - m, m);
+
+        // horizontally
+        int horizontalPlacement = placeTiles(n - 1, m);
+
+        return verticalPlacement + horizontalPlacement;
+    }
+
+    public static void main(String[] args) {
+        int n = 4, m = 2;
+        System.out.println(placeTiles(n, m));
+    }
+}
