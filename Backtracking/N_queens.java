b75a507f54bb81e6276e3a6d95f8f02c89fab7d4
Soumya ! <soumyadipadak99@gmail.com>
2024-06-30 20:26:53 +0530

    N Queens (important)

--- Backtracking/N_queens.java
+++ Backtracking/N_queens.java
@@ -4,33 +4,30 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class N_queens {
-
-    // isSafe function
     public boolean isSafe(int row, int col, char[][] board) {
-
-        // horizontal check
-        for (int j = 0; j < board.length; j++) {
-            if (board[row][j] == 'Q') {
+        // horizontal
+        for (int i = 0; i < board.length; i++) {
+            if (board[row][i] == 'Q') {
                 return false;
             }
         }
 
-        // vertical check
+        // vertiacal
         for (int i = 0; i < board.length; i++) {
             if (board[i][col] == 'Q') {
                 return false;
             }
         }
 
-        // upper left check
+        // upper left
         int r = row;
-        for (int c = col; c >= 0 && r >= 0; c--, r--) {
+        for (int c = col; c >= 0 && r >= 0; r--, c--) {
             if (board[r][c] == 'Q') {
                 return false;
             }
         }
 
-        // upper right check
+        // uper right
         r = row;
         for (int c = col; c < board.length && r >= 0; r--, c++) {
             if (board[r][c] == 'Q') {
@@ -38,7 +35,7 @@ public class N_queens {
             }
         }
 
-        // lower left check
+        // lower left
         r = row;
         for (int c = col; c >= 0 && r < board.length; r++, c--) {
             if (board[r][c] == 'Q') {
@@ -46,19 +43,16 @@ public class N_queens {
             }
         }
 
-        // lower right check
-        r = row;
+        // lower right
         for (int c = col; c < board.length && r < board.length; r++, c++) {
             if (board[r][c] == 'Q') {
                 return false;
             }
         }
-
         return true;
     }
 
-    // save board function
-    public void saveBoard(char[][] board, List<List<String>> allBoards) {
+    public void saveBoard(char[][] board, List<List<String>> allBoard) {
         List<String> newBoard = new ArrayList<>();
 
         for (int i = 0; i < board.length; i++) {
@@ -72,50 +66,47 @@ public class N_queens {
             }
             newBoard.add(row);
         }
-        allBoards.add(newBoard);
+        allBoard.add(newBoard);
     }
 
-    // backtracking function
-    public void helper(char[][] board, List<List<String>> allBoards, int col) {
-        // base condition
+    public void helper(char[][] board, List<List<String>> allBoard, int col) {
+        // base case
         if (col == board.length) {
-            saveBoard(board, allBoards);
+            saveBoard(board, allBoard);
             return;
         }
 
         for (int row = 0; row < board.length; row++) {
             if (isSafe(row, col, board)) {
                 board[row][col] = 'Q';
-                helper(board, allBoards, col + 1);
+                helper(board, allBoard, col + 1);
                 board[row][col] = '.';
             }
         }
     }
 
-    public List<List<String>> solveNQUeens(int n) {
-        List<List<String>> allBoards = new ArrayList<>();
+    public List<List<String>> solveNqueens(int n) {
+        List<List<String>> allBoard = new ArrayList<>();
         char[][] board = new char[n][n];
 
-        // Initialize the board with '.'
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 board[i][j] = '.';
             }
         }
-
-        helper(board, allBoards, 0);
-        return allBoards;
+        helper(board, allBoard, 0);
+        return allBoard;
     }
 
     public static void main(String[] args) {
-        N_queens nQueens = new N_queens();
+        N_queens n_queens = new N_queens();
         int n = 4;
-        List<List<String>> solutions = nQueens.solveNQUeens(n);
 
-        // Print all solutions
-        for (List<String> solution : solutions) {
-            for (String row : solution) {
-                System.out.print("[" + row + "]" + " ");
+        List<List<String>> solution = n_queens.solveNqueens(n);
+
+        for (List<String> a : solution) {
+            for (String result : a) {
+                System.out.print("[" + result + "]" + " ");
             }
             System.out.println();
         }
