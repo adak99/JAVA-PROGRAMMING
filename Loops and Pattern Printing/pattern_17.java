7faee4270d88e476fec246aea56d30df13828693
Soumya ! <soumyadipadak99@gmail.com>
2024-08-14 23:43:07 +0530

    Advanced pattern printing 17

---
+++ Loops and Pattern Printing/pattern_17.java
@@ -0,0 +1,25 @@
+//  & * * * * 
+//  * &     *
+//  *   &   *
+//  *     & *
+//  * * * * &
+
+public class pattern_17 {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) {
+                    System.out.print("&");
+                } else if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
