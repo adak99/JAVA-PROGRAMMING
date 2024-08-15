c0f5876aa0aaa15f324453d4967718105fa0bfe9
Soumya ! <soumyadipadak99@gmail.com>
2024-08-15 11:46:13 +0530

    Bianry search:

---
+++ Searching/BinarySearch.java
@@ -0,0 +1,34 @@
+public class BinarySearch {
+    public static int bianrySearch(int[] arr, int target) {
+        int strIdx = 0;
+        int endIdx = arr.length;
+
+        while (strIdx <= endIdx) {
+            int mid = strIdx + (endIdx - strIdx) / 2;
+
+            if (target < arr[mid]) {
+                mid = endIdx + 1;
+            } else if (target > arr[mid]) {
+                mid = strIdx - 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6 };
+        int target = 4;
+
+        int ans = bianrySearch(arr, target);
+
+        if (ans == -1) {
+            System.out.print("The target element not found");
+        } else {
+            System.out.println("The target element " + target + " found at index: " + ans);
+        }
+    }
+}
