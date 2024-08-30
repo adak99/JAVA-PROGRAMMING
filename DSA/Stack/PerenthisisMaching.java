7add02592a6ba743a425d531494cdc1a57b94776
Soumya ! <soumyadipadak99@gmail.com>
2024-08-30 20:49:29 +0530

    Perenthisis maching problem

---
+++ DSA/Stack/PerenthisisMaching.java
@@ -0,0 +1,59 @@
+public class PerenthisisMaching {
+    int size;
+    int top;
+    char[] arr;
+
+    PerenthisisMaching(int size) {
+        this.size = size;
+        this.top = -1;
+        this.arr = new char[size];
+    }
+
+    public void push(char val) {
+        if (top == size - 1) {
+            System.out.println("Stack is overflow.");
+            return;
+        }
+        arr[++top] = val;
+    }
+
+    public char pop() {
+        if (top == -1) {
+            System.out.println("Stack is underflow.");
+            return '\0';
+        }
+        char val = arr[top--];
+        return val;
+    }
+
+    public static boolean PerenthisisMaching_fun(String exp) {
+        PerenthisisMaching stack = new PerenthisisMaching(100);
+
+        for (int i = 0; i < exp.length(); i++) {
+            if (exp.charAt(i) == '(') {
+                stack.push(exp.charAt(i));
+            } else if (exp.charAt(i) == ')') {
+                if (stack.top == -1) {
+                    System.out.println("Stack is underflow.");
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+
+        if (stack.top == -1)
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+
+        String exp = "(8-1)*(8-2)";
+
+        if (PerenthisisMaching_fun(exp))
+            System.out.println("Perenthisis is maching.");
+        else
+            System.out.println("Perenthisis is not maching.");
+    }
+}
