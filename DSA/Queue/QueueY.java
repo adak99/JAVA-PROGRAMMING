09d2f45713f08abe82c4eff095b45359883c672b
Soumya ! <soumyadipadak99@gmail.com>
2024-09-26 02:22:48 +0530

    Queue implementing using array

--- DSA/Queue/QueueY.java
+++ DSA/Queue/QueueY.java
@@ -2,11 +2,12 @@ public class QueueY {
     static class queue {
         static int arr[];
         static int size;
-        static int rear = -1;
+        static int rear;
 
         queue(int n) {
             arr = new int[n];
             size = n;
+            rear = -1;
         }
 
         public boolean isEmpty() {
@@ -14,31 +15,31 @@ public class QueueY {
         }
 
         // enqueue
-        public void add(int data) {
+        public void enqueue(int data) {
             if (rear == size - 1) {
-                System.err.println("Queue is full.");
+                System.out.println("Queue is full.");
                 return;
             }
 
             arr[++rear] = data;
+            System.out.println("Enqueue: " + data);
         }
 
-        // dequeque
-        public int remove() {
+        // dequeue
+        public int dequeque() {
             if (isEmpty()) {
                 System.out.println("Queue is empty.");
                 return -1;
             }
 
-            int front = arr[0];
+            int fornt = arr[0];
             for (int i = 0; i < rear; i++) {
                 arr[i] = arr[i + 1];
             }
             rear--;
-            return front;
+            return fornt;
         }
 
-        // peek
         public int peek() {
             if (isEmpty()) {
                 System.out.println("Queue is empty.");
@@ -50,16 +51,16 @@ public class QueueY {
     }
 
     public static void main(String[] args) {
-        queue q = new queue(5);
+        queue q = new queue(3);
 
-        q.add(10);
-        q.add(20);
-        q.add(30);
-        q.add(40);
+        for (int i = 10; i <= 40; i += 10) {
+            q.enqueue(i);
+        }
 
+        System.out.print("Queue is element: ");
         while (!q.isEmpty()) {
-            System.out.println(q.peek());
-            q.remove();
+            System.out.print(q.peek() + " ");
+            q.dequeque();
         }
     }
 }
