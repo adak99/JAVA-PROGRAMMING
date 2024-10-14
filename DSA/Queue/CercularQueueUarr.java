8b0516e533493c557f8f9b3f162eb4a6030c8b9a
Soumya ! <soumyadipadak99@gmail.com>
2024-10-14 13:56:35 +0530

    Cercular Queue

--- DSA/Queue/CercularQueueUarr.java
+++ DSA/Queue/CercularQueueUarr.java
@@ -1,31 +1,35 @@
-public class CercularQueue {
+public class CercularQueueUarr {
     static class queue {
-        static int arr[];
+        static int[] arr;
         static int size;
-        static int rear = -1;
-        static int front = -1;
+        static int front;
+        static int rear;
 
         queue(int n) {
             arr = new int[n];
             size = n;
+            front = -1;
+            rear = -1;
         }
 
-        public static boolean isEmpty() {
+        // is empty condition
+        public boolean isEmpty() {
             return rear == -1 && front == -1;
         }
 
-        public static boolean isFull() {
+        // is full condition
+        public boolean isFull() {
             return (rear + 1) % size == front;
         }
 
         // enqueue
-        public static void add(int data) {
+        public void add(int data) {
             if (isFull()) {
-                System.err.println("Queue is full.");
+                System.out.println("Queue is full.");
                 return;
             }
 
-            if (front == -1) {
+            if (front == -1) { // if add first element front value increase by -1 to 0
                 front = 0;
             }
 
@@ -33,15 +37,15 @@ public class CercularQueue {
             arr[rear] = data;
         }
 
-        // dequeque
-        public static int remove() {
+        // dequeue
+        public int remove() {
             if (isEmpty()) {
-                System.out.println("Queue is empty.");
+                System.out.println("Stack is empty.");
                 return -1;
             }
 
             int result = arr[front];
-
+            // if queue have a singel element the rear and front set into previous state -1
             if (rear == front) {
                 rear = front = -1;
             } else {
@@ -51,30 +55,28 @@ public class CercularQueue {
             return result;
         }
 
-        // peek
-        public static int peek() {
+        // peek function
+        public int peek() {
             if (isEmpty()) {
-                System.out.println("Queue is empty.");
+                System.out.println("Stack is empty.");
                 return -1;
             }
-
             return arr[front];
         }
     }
 
     public static void main(String[] args) {
-        queue q = new queue(5);
+        queue q = new queue(4);
 
         q.add(10);
         q.add(20);
         q.add(30);
         q.add(40);
         q.add(50);
-        System.out.println("remove: " + q.remove());
-        q.add(60);
-        System.out.println("remove: " + q.remove());
-        q.add(70);
 
+        System.out.println("Peek element: " + q.peek());
+        System.out.println("Remove element: " + q.remove());
+        System.out.println("Display all element.");
         while (!q.isEmpty()) {
             System.out.println(q.peek());
             q.remove();
