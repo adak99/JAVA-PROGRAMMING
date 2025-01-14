f3ded3f5b12cc95db97137a1255cd1fdbf2beee1
Soumyadip Adak <soumyadipadak99@gmail.com>
2025-01-14 18:35:32 +0530

    Bianry Tree

--- DSA/Binary Tree/src/DiameterOfTree.java
+++ DSA/Binary Tree/src/DiameterOfTree.java
@@ -1,27 +1,14 @@
 import buildtree.BuildTree;
 import createNode.Node;
-import myfunction.TreeHeight;
+import myfunction.MyFunctions;
 
 public class DiameterOfTree {
-
-    public static int diameter(Node root) {
-        if (root == null) {
-            return 0;
-        }
-
-        TreeHeight h = new TreeHeight();
-        int diameter1 = diameter(root.left);
-        int diameter2 = diameter(root.right);
-        int diameter3 = h.height(root.left) + h.height(root.right) + 1;
-
-        return Math.max(diameter3, Math.max(diameter1, diameter2));
-    }
-
     public static void main(String[] args) {
         int[] nodes = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         BuildTree tree = new BuildTree();
+        MyFunctions h = new MyFunctions();
         Node root = tree.buildTree(nodes);
 
-        System.out.println("Diameter of the tree: " + diameter(root));
+        System.out.println("Diameter of the tree: " + h.diameter(root));
     }
 }
--- DSA/Binary Tree/src/HeightOfTree.java
+++ DSA/Binary Tree/src/HeightOfTree.java
@@ -1,12 +1,12 @@
 import buildtree.BuildTree;
 import createNode.Node;
-import myfunction.TreeHeight;
+import myfunction.MyFunctions;
 
 public class HeightOfTree {
     public static void main(String[] args) {
         int[] nodes = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         BuildTree tree = new BuildTree();
-        TreeHeight h = new TreeHeight();
+        MyFunctions h = new MyFunctions();
         Node root = tree.buildTree(nodes);
         System.out.println("Hight of the tree: " + h.height(root));
     }
