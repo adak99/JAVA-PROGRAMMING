3572d6e96d0503ba6bfce84869520122449592ea
Soumyadip Adak <soumyadipadak99@gmail.com>
2025-01-24 19:40:18 +0530

    Print in range

--- DSA/Binary Tree/src/BainarySearchTree/PrintInRange.java
+++ DSA/Binary Tree/src/BainarySearchTree/PrintInRange.java
@@ -4,6 +4,36 @@ import BainarySearchTree.BuildTree.CreateTree;
 import BainarySearchTree.CreateNode.Node;
 
 public class PrintInRange {
+
+    // approach
+    /*
+     * range x and y
+     * 
+     * case 1: if x present & equal to both range in right sub tree and y present in
+     * left subtree-> [x <= root <= y] -> left sutree && -> right subtree call
+     * 
+     * case 2: if the X is greater than root then all subtree&nodes found at the
+     * right subtree-> [x > root] -> right
+     * 
+     * case 2: [y < root] -> left
+     */
+
+    public static void printInRnage(Node root, int x, int y) {
+        if (root == null) {
+            return;
+        }
+
+        if (root.data >= x && root.data <= y) {
+            printInRnage(root.left, x, y);
+            System.out.print(root.data + " ");
+            printInRnage(root.right, x, y);
+        } else if (root.data >= y) {
+            printInRnage(root.left, x, y);
+        } else {
+            printInRnage(root.right, x, y);
+        }
+    }
+
     public static void main(String[] args) {
         int nodes[] = { 8, 5, 3, 1, 4, 6, 10, 11, 14 };
         Node root = null;
@@ -11,6 +41,8 @@ public class PrintInRange {
         for (int key : nodes) {
             root = CreateTree.insert(root, key);
         }
+
+        printInRnage(root, 8, 14);
 
     }
 }
