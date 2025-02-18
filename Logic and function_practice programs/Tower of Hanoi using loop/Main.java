aa094a72ae8fa5c79d2dfb4befc485236e9bceaf
Soumyadip Adak <149286607+adak99@users.noreply.github.com>
2025-02-18 22:50:56 +0530

    Tower of hanoi using loop

---
+++ Logic and function_practice programs/Tower of Hanoi using loop/Main.java
@@ -0,0 +1,73 @@
+public class Main {
+    // Implement stack
+    static class Stack<K> {
+        private class Node {
+            K data;
+            Node next;
+
+            Node(K data) {
+                this.data = data;
+                next = null;
+            }
+        }
+
+        private Node head = null;
+
+        public boolean isEmpty() {
+            return head == null;
+        }
+
+        public void push(K data) {
+            Node newNode = new Node(data);
+            newNode.next = head;
+            head = newNode;
+        }
+
+        public K pop() {
+            if (isEmpty()) {
+                System.out.println("Stack is empty.");
+                return null;
+            }
+            K popVal = head.data;
+            head = head.next;
+            return popVal;
+        }
+    }
+
+    static class Move {
+        int disk;
+        String source, destination;
+
+        Move(int disk, String source, String destination) {
+            this.disk = disk;
+            this.source = source;
+            this.destination = destination;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("Move disk %s to %s.", source, destination);
+        }
+    }
+
+    public static void towerOfHanoi(int n, String source, String helper, String destination) {
+        Stack<Move> stack = new Stack<>();
+        stack.push(new Move(n, source, destination));
+
+        while (!stack.isEmpty()) {
+            Move currentMove = stack.pop();
+
+            if (currentMove.disk == 1) {
+                System.out.println(currentMove);
+            } else {
+                stack.push(new Move(currentMove.disk - 1, helper, destination));
+                stack.push(new Move(1, source, destination));
+                stack.push(new Move(currentMove.disk - 1, source, helper));
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        towerOfHanoi(3, "SCOURCE", "DESTIONATION", "DESTINATION");
+    }
+}
